693e061c51a55dccab5362dfc95e7b366b91b2aa
Travis Molter <travismolter@gmail.com>
2019-10-07 16:52:51 -0700

    created a jewelry list

---
+++ jewelry.js
@@ -0,0 +1,44 @@
+const turquoiseRing = {
+    id: 'turquoise-ring',
+    name: 'Turquoise Ring',
+    image: 'turquoisering.jpg',
+    description: 'Handmade Turquoise Ring',
+    category: 'ring',
+    price: 50.00
+};
+
+const blackTurquoiseRing = {
+    id: 'black-turquoise-ring',
+    name: 'Black Turquoise Ring',
+    image: 'blackturquoisering.jpg',
+    description: 'Handmade Black Turquoise Ring',
+    category: 'ring',
+    price: 50.00
+};
+
+const turquoiseBracelet = {
+    id: 'turquoise-bracelet',
+    name: 'Turquoise Bracelet',
+    image: 'turquoisebracelet.jpg',
+    description: 'Handmade Turquoise Bracelet',
+    category: 'bracelet',
+    price: 45.00
+};
+
+const turquoiseNecklace = {
+    id: 'turquoise-necklace',
+    name: 'Turquoise Necklace',
+    image: 'turquoisenecklace.jpg',
+    description: 'Handmade Turquoise Necklace',
+    category: 'necklace',
+    price: 45.00
+};
+
+const jewelry = [
+    turquoiseRing,
+    blackTurquoiseRing,
+    turquoiseBracelet,
+    turquoiseNecklace
+];
+
+export default jewelry;
