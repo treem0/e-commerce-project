2e97d25b74984aa7a88e4d715c49269406f9c6f5
Travis Molter <travismolter@gmail.com>
2019-10-08 16:10:11 -0700

    started to make a shopping cart but need to create new branch and work from there

--- Common/Utils.js
+++ Common/Utils.js
@@ -1,4 +1,14 @@
+import jewelries from "../jewelry";
+
 export const makePrettyCurrency = (number) => number.toLocaleString('en-US', {
     style: 'currency',
     currency: 'USD',
 });
+
+const totalJewelryPrice = (jewelries, quantity) => jewelries.price * quantity;
+
+}
+export const totalCartTotal = (jewelries, cart) => {
+    
+
+}
