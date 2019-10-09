c147d71208ee486b3f365b6f30bbbd4027c57446
Travis Molter <travismolter@gmail.com>
2019-10-08 17:54:31 -0700

    started to work on test for findItemById function

--- Common/Utils.js
+++ Common/Utils.js
@@ -1,4 +1,7 @@
-import jewelries from "../jewelry";
+import jewelries from '../jewelry.js';
+import cart from '../Data/cart.js';
+
+const tableElement = document.querySelector('tbody');
 
 export const makePrettyCurrency = (number) => number.toLocaleString('en-US', {
     style: 'currency',
@@ -7,8 +10,18 @@ export const makePrettyCurrency = (number) => number.toLocaleString('en-US', {
 
 const totalJewelryPrice = (jewelries, quantity) => jewelries.price * quantity;
 
-}
+
 export const totalCartTotal = (jewelries, cart) => {
     
+};
 
-}
+
+export const findItemById = (jewelries, id) => {
+    for (let i = 0; i < jewelries.length; i++) {
+        const lineItem = cart[i];
+        const jewelry = findItemById(jewelries, lineItem.id);
+        const dom = renderTableRow(lineItem, jewelry);
+
+        tableElement.appendChild(dom);
+    }
+};
--- test/render-jewelry.test.js
+++ test/render-jewelry.test.js
@@ -2,6 +2,7 @@
 // import example from '../src/example.js';
 import renderJewelry from '../Products/render-jewelry.js';
 import renderTableRow from '../Shopping Cart/render-table-row.js';
+import { findItemById } from '../Common/Utils.js';
 
 const test = QUnit.test;
 
@@ -28,6 +29,31 @@ test('time to test a function', function(assert) {
 });
 
 test('renders a table row', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+
+    const blackTurquoiseRing = {
+        id: 'black-turquoise-ring',
+        name: 'Black Turquoise Ring',
+        image: 'blackturquoisering.jpg',
+        description: 'Handmade Black Turquoise Ring',
+        category: 'ring',
+        price: 50.00
+    };
+    const blackTurquoiseRingOrder = {
+        id: 'black-turquoise-ring',
+        quantity: 2
+    };
+    const expected = '<tr><td>Black Turquoise Ring</td><td>2</td><td>$50.00</td><td>$100.00</td></tr>';
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const jewelryElementTr = renderTableRow(blackTurquoiseRing, blackTurquoiseRingOrder);
+    const stringHtmlOfJewelryElement = jewelryElementTr.outerHTML;
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(stringHtmlOfJewelryElement, expected);
+});
+test('take an array and returns first item with ID', function(assert) {
     //Arrange
     // Set up your parameters and expectations
 
