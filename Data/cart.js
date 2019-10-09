e86d2f4fb37cea189443823b9ff2e94f563428d9
Travis Molter <travismolter@gmail.com>
2019-10-08 17:12:40 -0700

    created a cart.js file and added some data

---
+++ Data/cart.js
@@ -0,0 +1,15 @@
+const cart = [{
+    id: 'turquoise-ring',
+    quantity: 2
+}, {
+    id: 'black-turquoise-ring',
+    quantity: 5
+}, {
+    id: 'turquoise-bracelet',
+    quantity: 1
+}, {
+    id: 'turquoise-necklace',
+    quantity: 10
+}];
+
+export default cart;
